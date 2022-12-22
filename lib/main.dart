7cfbcaeab986a20ca32a0eb82526b97ad8b9a37d
Marcos Mantovani <mantovani.marcos@gmail.com>
2022-12-22 12:19:08 -0300

    Added a launch.json that includes RELEASE build.

--- lib/main.dart
+++ lib/main.dart
@@ -37,7 +37,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      debugShowCheckedModeBanner: false,
+      //debugShowCheckedModeBanner: false,
       home: HomePage(),
     );
   }
