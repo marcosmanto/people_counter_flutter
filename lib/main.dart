df1c29b1a8ea164f99fb3e3562cd5408ec7a2141
Marcos Mantovani <mantovani.marcos@gmail.com>
2022-12-21 23:54:15 -0300

    Working on Windows now with custom window frame and working in web using isNotWeb() function.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,33 @@
+import 'dart:io';
+
+import 'package:bitsdojo_window/bitsdojo_window.dart';
 import 'package:flutter/material.dart';
+
+bool isNotWeb() {
+  try {
+    Platform.isWindows;
+  } catch (error) {
+    return false;
+  }
+  return true;
+}
 
 void main(List<String> args) {
   runApp(const MyApp());
+
+  if (isNotWeb()) {
+    if (Platform.isWindows) {
+      doWhenWindowReady(() {
+        const initialSize = Size(730, 580);
+        appWindow.minSize = initialSize;
+        appWindow.size = initialSize;
+        appWindow.maxSize = initialSize;
+        appWindow.alignment = Alignment.center;
+        appWindow.title = 'People Counter';
+        appWindow.show();
+      });
+    }
+  }
 }
 
 // Widget holds all App's config and initialization
@@ -46,95 +72,130 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            fit: BoxFit.cover,
-            image: AssetImage('assets/images/ice-cream-contrast.png'),
-          ),
-        ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Column(
+      body: Stack(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                fit: BoxFit.cover,
+                image: AssetImage('assets/images/ice-cream-contrast.png'),
+              ),
+            ),
+            child: Row(
               mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Card(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(24),
-                  ),
-                  shadowColor: Colors.transparent,
-                  color: isFull
-                      ? const Color.fromARGB(170, 244, 67, 54)
-                      : HomePage.whiteColor,
-                  child: Container(
-                    alignment: Alignment.center,
-                    width: HomePage.maxSize,
-                    height: HomePage.maxSize,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          isFull ? 'Lotado' : 'Pode entrar!',
-                          style: TextStyle(fontSize: 30, color: cardTextColor),
+              children: [
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Card(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(24),
+                      ),
+                      shadowColor: Colors.transparent,
+                      color: isFull
+                          ? const Color.fromARGB(170, 244, 67, 54)
+                          : HomePage.whiteColor,
+                      child: Container(
+                        alignment: Alignment.center,
+                        width: HomePage.maxSize,
+                        height: HomePage.maxSize,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              isFull ? 'Lotado' : 'Pode entrar!',
+                              style:
+                                  TextStyle(fontSize: 30, color: cardTextColor),
+                            ),
+                            Text(count.toString(),
+                                style: TextStyle(
+                                    fontSize: 100, color: cardTextColor))
+                          ],
                         ),
-                        Text(count.toString(),
-                            style:
-                                TextStyle(fontSize: 100, color: cardTextColor))
-                      ],
+                      ),
                     ),
-                  ),
-                ),
-                Container(
-                  width: HomePage.maxSize,
-                  margin: const EdgeInsets.only(top: 20),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: HomePage.whiteColor,
-                          shadowColor: Colors.transparent,
-                          fixedSize: const Size(120, 100),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(24),
+                    Container(
+                      width: HomePage.maxSize,
+                      margin: const EdgeInsets.only(top: 20),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: HomePage.whiteColor,
+                              shadowColor: Colors.transparent,
+                              fixedSize: const Size(120, 100),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(24),
+                              ),
+                            ),
+                            onPressed: isEmpty ? null : decrement,
+                            child: const Text(
+                              'Saiu',
+                              style: TextStyle(
+                                fontSize: 24,
+                                color: Colors.black,
+                              ),
+                            ),
                           ),
-                        ),
-                        onPressed: isEmpty ? null : decrement,
-                        child: const Text(
-                          'Saiu',
-                          style: TextStyle(
-                            fontSize: 24,
-                            color: Colors.black,
+                          ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: HomePage.whiteColor,
+                              shadowColor: Colors.transparent,
+                              fixedSize: const Size(120, 100),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(24),
+                              ),
+                            ),
+                            onPressed: isFull ? null : increment,
+                            child: const Text(
+                              'Entrou',
+                              style: TextStyle(
+                                fontSize: 24,
+                                color: Colors.black,
+                              ),
+                            ),
                           ),
-                        ),
+                        ],
                       ),
-                      ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: HomePage.whiteColor,
-                          shadowColor: Colors.transparent,
-                          fixedSize: const Size(120, 100),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(24),
-                          ),
-                        ),
-                        onPressed: isFull ? null : increment,
-                        child: const Text(
-                          'Entrou',
-                          style: TextStyle(
-                            fontSize: 24,
-                            color: Colors.black,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
               ],
             ),
-          ],
-        ),
+          ),
+          if (isNotWeb() && Platform.isWindows)
+            Column(
+              children: [
+                WindowTitleBarBox(
+                  child: Row(
+                    children: [
+                      Expanded(child: MoveWindow()),
+                      const WindowButtons(),
+                    ],
+                  ),
+                )
+              ],
+            ),
+        ],
       ),
     );
+  }
+}
+
+var buttonColors = WindowButtonColors(
+  mouseOver: Color(0xFF0F829D),
+  mouseDown: Color(0xFFDE318D),
+);
+
+class WindowButtons extends StatelessWidget {
+  const WindowButtons({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(children: [
+      MinimizeWindowButton(colors: buttonColors),
+      CloseWindowButton(),
+    ]);
   }
 }
